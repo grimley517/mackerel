6963875b7fbe6c6a25149dcee06105567208a857
Brian Jones <ba.jones@me.com>
2015-01-15 05:18:20 +0000

    started test

---
+++ test_mackerel.py
@@ -0,0 +1,16 @@
+import unittest
+import makerel
+
+class TestSequenceFunctions(unittest.TestCase):
+
+    def setUp(self):
+        pass
+    
+    def test1(self):
+        
+        pass
+
+    
+
+if __name__ == '__main__':
+    unittest.main(verbosity = 2)
